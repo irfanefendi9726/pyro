0f30fecf9ccf146fa2bf0f5ecba67e7494cb1d36
irfanefendi9726 <irfan.efendi@pipeline-network.com>
2021-12-21 13:07:46 +0800

    push

--- resources/views/dua.blade.php
+++ resources/views/dua.blade.php
@@ -131,12 +131,8 @@
                 </div>
             </div>
         </div>
-        <div class="lg-4 text-center text-sm text-black-600 sm:text-right sm:ml-0">
-
-
-            Termination Process ready to execute
-    
-    
-        </div>
+            <div class="lg-4 text-center text-sm text-black-600">
+                 Termination Process ready to execute
+            </div>
     </body>
 </html>

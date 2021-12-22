f863f59c6bc6055ebb37446a5f3d8cb53bef3167
irfanefendi9726 <irfan.efendi@pipeline-network.com>
2021-12-22 12:19:45 +0800

    push

--- resources/views/tiga.blade.php
+++ resources/views/tiga.blade.php
@@ -5,6 +5,10 @@ html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>MyPapanTanda</title>
+        <title>Prototype Example</title>
+        <link rel = "icon" href = 
+        "https://media.geeksforgeeks.org/wp-content/cdn-uploads/gfg_200X200.png" 
+        type = "image/x-icon">
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,10 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>MyPapanTanda</title>
+        <title>Prototype Example</title>
+        <link rel = "icon" href = 
+        "https://media.geeksforgeeks.org/wp-content/cdn-uploads/gfg_200X200.png" 
+        type = "image/x-icon">
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">

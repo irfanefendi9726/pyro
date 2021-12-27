00d6941f858484024ccda1801b8eab7398d76b50
irfanefendi9726 <irfan.efendi@pipeline-network.com>
2021-12-27 11:12:25 +0800

    push

--- database/migrations/2021_12_27_022543_create_products_table.php
+++ database/migrations/2021_12_27_022543_create_products_table.php
@@ -15,6 +15,8 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->name();
+            $detail->detail();
             $table->timestamps();
         });
     }

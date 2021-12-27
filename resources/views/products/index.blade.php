16219f185d12c129132acdaf6c7e73820575f0ee
irfanefendi9726 <irfan.efendi@pipeline-network.com>
2021-12-27 12:12:33 +0800

    change form

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -4,7 +4,7 @@
     <div class="row">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
-                <h2>Laravel 8 CRUD Example from scratch - ItSolutionStuff.com</h2>
+                <h2>Submit your name and details here and we can proceed with the solution</h2>
             </div>
             <div class="pull-right">
                 <a class="btn btn-success" href="{{ route('products.create') }}"> Create New Product</a>
